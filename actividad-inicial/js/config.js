b1cf81926f7a48fe2b81ee00eeed98d4799c6956
David López Castellote <david.lopez.castellote@gmail.com>
2026-09-07 21:34:04 +0200

    actualizado apps script url

--- actividad-inicial/js/config.js
+++ actividad-inicial/js/config.js
@@ -10,7 +10,7 @@ const APP_CONFIG = {
   // Pega aquí la URL HTTP POST que te proporcione el flujo de Power Automate.
   // Si se deja vacía (''), la web simulará el guardado correctamente y permitirá
   // descargar el carnet para subirlo a Aules.
-  EXCEL_WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbzMfzfcXqQO-2f5NnNzotANaP2AiVOy9Nu4SOS3DPshF1pYBO2wmeREobl2XjRL2zO3/exec',
+  EXCEL_WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbzn98yPcpw9Bnl3EAINLTuWQE07rAQs3rlnD-bdcY6aTh0XDmK84XAjCbr9RsN9ViFU/exec',
 
   // Nombre de la institución o centro (opcional)
   CENTRO_EDUCATIVO: 'Dpto. de Informática',
